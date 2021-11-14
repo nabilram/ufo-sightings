0e19d06c727a5f39336527ecf830a06f9fc3a283
nabilram <89229716+nabilram@users.noreply.github.com>
2021-11-14 22:10:06 +0100

    11.6.1

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 const tableData = data;
 
 // Reference the HTML table using d3
-//d3.select is tell JS to look for <tbody> tags in HTML
+// d3.select is tell JS to look for <tbody> tags in HTML
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
@@ -14,4 +14,18 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value"); // grab the date time value from filter (input by user?)
+    let filteredData = tableData; // reset tale to original state; assign the constant global table to it
+    if (date) { //if there is a date entered -- there is a value on date
+        filteredData = filteredData.filter(row => row.datetime === date); // filter all rows that have strict equality to date
+    };
+    buildTable(filteredData); //build the table using filtered data; unfiltered data b/c no date makes it orig table
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick); // listen to event; call out unique button element that you will assign
+
+buildTable(tableData); // build table when page loads -- techinically when the code itself starts
+
